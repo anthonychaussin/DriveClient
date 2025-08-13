bf3785204a9649cd58ab75b99aac8056d09c2e91
anthonychaussin <chaussin.anthony@gmail.com>
2025-08-13 13:32:24 +0200

    add conflict choice and user_agent

--- DriveClient/Models/ConflictChoice.cs
+++ DriveClient/Models/ConflictChoice.cs
@@ -1,15 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace kDriveClient.Models
+﻿namespace kDriveClient.Models
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public enum ConflictChoice
     {
+        /// <summary>
+        /// Throw an error
+        /// </summary>
         Error,
+        /// <summary>
+        /// Add a new version to file
+        /// </summary>
         Version,
+        /// <summary>
+        /// Create a new file with an automated renaming
+        /// </summary>
         Rename,
     }
 }
